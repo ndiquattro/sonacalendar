e49636435cbd60fb9e9ffb47804bba8eb5a8dfad
Nick DiQuattro <ndiquattro@gmail.com>
2015-08-30 20:42:21 -0700

    First commit

---
+++ gcal.py
@@ -0,0 +1,107 @@
+# Class for interacting with google calendar
+import rfc3339
+import httplib2
+from apiclient import discovery
+import oauth2client
+from oauth2client import client
+from oauth2client import tools
+import os
+import datetime
+
+# try:
+#     import argparse
+#     flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
+# except ImportError:
+#     flags = None
+
+
+# Make Class
+class calMaster(object):
+
+    # Initiate
+    def __init__(self):
+
+        # Authorize and create service
+        credentials = self.get_credentials()
+        http = credentials.authorize(httplib2.Http())
+        self.service = discovery.build('calendar', 'v3', http=http)
+
+    # Authorize with Google API
+    def get_credentials(self):
+
+            # Set variables
+            scopes = 'https://www.googleapis.com/auth/calendar'
+            client_secret_file = 'googleauth.json'
+            application_name = 'sonacalendar'
+            credential_path = os.path.join(os.getcwd(), 'goauth.json')
+
+            # Try to load stored credentials
+            store = oauth2client.file.Storage(credential_path)
+            credentials = store.get()
+
+            # Get new if invalid or not there
+            if not credentials or credentials.invalid:
+                flow = client.flow_from_clientsecrets(client_secret_file,
+                                                      scopes)
+                flow.user_agent = application_name
+
+                credentials = tools.run_flow(flow, store, flags)
+
+            return credentials
+
+    # Scrape Calendar events
+    def calevents(self, calid):
+
+        # Grab Events
+        now = datetime.datetime.utcnow().isoformat() + 'Z'
+        eventsresult = self.service.events().list(
+            calendarId=calid, timeMin=now, singleEvents=True,
+            orderBy='startTime').execute()
+        events = eventsresult.get('items', [])
+
+        # Parse Events
+        cevents = []
+        for event in events:
+            start = rfc3339.parse_datetime(event['start']['dateTime'])
+            cevents.append({'name': event['summary'],
+                            'start': start,
+                            'eid': event['id']})
+
+        return cevents
+
+    def mkevent(self, vals, calid, system):
+
+        event = {'summary': 'Nick - {}'.format(vals['name']),
+                 'description': system,
+                 'start': {
+                     'dateTime': rfc3339.datetimetostr(vals['start'])[:-1],
+                     'timeZone': 'America/Los_Angeles'},
+                 'end': {
+                     'dateTime': rfc3339.datetimetostr(vals['end'])[:-1],
+                     'timeZone': 'America/Los_Angeles'}
+                 }
+
+        # Add event
+        self.service.events().insert(calendarId=calid,
+                                body=event).execute()
+
+    def clearcal(self, calid):
+
+        # Get list of events
+        events = self.service.events().list(calendarId=calid).execute()
+        events = events.get('items', [])
+
+        # Delete them
+        for event in events:
+            self.service.events().delete(calendarId=calid,
+                                         eventId=event['id']).execute()
+
+    def update_event(self, calid, eid, new_name):
+
+        # New event dict
+        name = 'Nick - {}'.format(new_name)
+        nevent = {'summary': name}
+
+        # Update
+        self.service.events().update(calendarId=calid, eventId=eid,
+                                     event=nevent).execute()
---
+++ sonacalendar.py
@@ -0,0 +1,122 @@
+from bs4 import BeautifulSoup
+import mechanize
+import datetime
+import os
+import yaml
+import gcal
+import urlparse
+
+#gcal = gcal.calMaster()
+#gcal.clearcal('i347pba0mkqe0trvfujsjb92ts@group.calendar.google.com')
+
+# Scrape Sona
+def scrape_slots(username, password, url):
+    # Browser
+    br = mechanize.Browser()
+
+    # Settings
+    br.set_handle_robots(False)
+    br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
+
+    # Open page and select login form
+    mainpage = urlparse.urlparse(url)
+    mainpage = '{0}://{1}'.format(mainpage.scheme, mainpage.netloc)
+    br.open(mainpage)
+    br.select_form(name="aspnetForm")
+
+    # Fill out and submit form
+    br["ctl00$ContentPlaceHolder1$userid"] = username
+    br["ctl00$ContentPlaceHolder1$pw"] = password
+    br.submit()
+
+    # Open studies page
+    slots_html = br.open(url)
+
+    # Soupify
+    soup = BeautifulSoup(slots_html, "html.parser")
+
+    # Get the studies rows
+    slot_rows = soup.select("table tbody tr")
+
+    # Date Parser
+    def pdate(dstr):
+
+        # Split and remove blanks
+        clean_str = filter(None, dstr.split('\n'))
+
+        # Make start and end strings
+        day = '{1}{2}'.format(*tuple(clean_str[0].split(','))).lstrip()
+        start = clean_str[1].split(' - ')[0]
+        end = clean_str[1].split(' - ')[1]
+        dtime = '{} {}'.format(day, start)
+        dtimend = '{} {}'.format(day, end)
+        dtime = datetime.datetime.strptime(dtime, '%B %d %Y %I:%M %p')
+        dtimend = datetime.datetime.strptime(dtimend, '%B %d %Y %I:%M %p')
+
+        return dtime, dtimend
+
+    # Parse studies
+    slots = []
+    for row in slot_rows:
+        columns = row.find_all('td')
+        start, end = pdate(columns[0].get_text())
+        name = columns[2].get_text().split('\n')[1]
+
+        slots.append({'name': name,
+                      'start': start,
+                      'end': end})
+
+    return slots
+
+
+def main(sonasystem, einfo):
+    try:
+
+        # Scrape slots
+        signups = scrape_slots(einfo['user'],
+                               einfo['pass'],
+                               einfo['url'])
+
+        # Scrape Calendar
+        events = gcal.calevents(einfo['calid'])
+
+        # Iterate through slots
+        for slot in signups:
+            # Grab corresponding calendar event
+            cevent = []
+            cevent = [event for event in events
+                      if event['start'].replace(tzinfo=None) == slot['start']]
+
+            if cevent:
+                # Select subject name
+                sname = cevent[0]['name'].split(' - ')[1]
+                print 'not make'
+
+                # Check that names match
+                if slot['name'] != sname:
+                    gcal.update_event(einfo['calid'], slot['eid'], slot['name'])
+                    print 'updated'
+            else:
+                gcal.mkevent(slot, einfo['calid'], sonasystem)
+                print 'make'
+
+        # Log it
+        with open(str(os.getcwd()) + '/log.txt', 'a') as f:
+            f.write(str(datetime.datetime.now()) + ",success\n")
+
+    except Exception, e:
+        # Log it
+        with open(str(os.getcwd()) + '/err.txt', 'a') as f:
+            f.write(str(datetime.datetime.now()) + "," + str(e) + "\n")
+
+
+if __name__ == "__main__":
+    # Load secrets
+    with open('config.yaml', 'r') as f:
+        config = yaml.load(f)
+
+    # Initiate Google Calendar Interface
+    gcal = gcal.calMaster()
+
+    for key, val in config.iteritems():
+        main(key, val)
